62650fcde3b3a8c5479e87427f6fa6781f6cfaab
Shreyash Kumar Singh <shreyash.ksingh.cd.eee19@itbhu.ac.in>
2021-03-12 02:00:47 +0530

    set up redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
 
+import { store, persistor } from './redux/store';
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store} >
+    <PersistGate persistor = {persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
